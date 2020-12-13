20d66fb73846e4c26a0d6d671750614d17b4da96
issam <issamyayou@gmail.com>
2020-12-13 19:56:23 +0100

    update show.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Google_Service_Drive_DriveFile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
@@ -34,6 +35,19 @@ class UploadController extends Controller
       
         // Store inGoogle Drive folder
         $pathToFile = $request->pdfile->storeAs(config('folderId'), $filename, 'google');
+
+
+        
+        // This is uploading a file directly, with no metadata associated.
+        // $file = new Google_Service_Drive_DriveFile();
+        // $result = $service->files->create(
+        //     $file,
+        //     array(
+        //         'data' => file_get_contents(TESTFILE),
+        //         'mimeType' => 'application/octet-stream',
+        //         'uploadType' => 'media'
+        //     )
+        // );
 
         // store localy
         // $pathToFile = $request->pdfile->storeAs('files', $filename, 'public');
