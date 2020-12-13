2314f20a547b8126c7d7baafc6c800a15e3cb5d0
issam <issamyayou@gmail.com>
2020-12-13 18:10:44 +0100

    update show.

--- resources/views/files/show.blade.php
+++ resources/views/files/show.blade.php
@@ -5,12 +5,13 @@
 <div class="container">
 
     <h1>Show File From Google Drive</h1>
+    <h3>{{ $file }}</h3>
 
-    {{-- <a href="{{ asset($file) }}">Open the pdf!</a> --}}
+    <a href="https://drive.google.com/file/d/1TpvRPzsFgJl-qrk1N7uecGMwoBYwvijn/view" target="_blank">open the pdf!</a>
 
-    <iframe data-src="https://drive.google.com/file/d/1NsYORz__A-PJImUYtPe-a9CnQ9Ro1hlx/preview"
+    <iframe data-src="https://drive.google.com/file/d/1TpvRPzsFgJl-qrk1N7uecGMwoBYwvijn/preview"
     {{-- src="{{ storage_path('app/public' . $file ) }}"  --}}
-    src="https://drive.google.com/file/d/1NsYORz__A-PJImUYtPe-a9CnQ9Ro1hlx/preview"
+    src="https://drive.google.com/file/d/1TpvRPzsFgJl-qrk1N7uecGMwoBYwvijn/preview"
     type="application/pdf" style="width: 100%; height: 75vh">
     </iframe>
 
