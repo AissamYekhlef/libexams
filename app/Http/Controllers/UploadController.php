0e1ffed45e23f91cc7d83388b19d45f306a09618
issam <issamyayou@gmail.com>
2020-12-13 20:14:11 +0100

    fix error get fileId ofinserted file.

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Google_Client;
 use Google_Service_Drive_DriveFile;
+use Hypweb\Flysystem\GoogleDrive\GoogleDriveAdapter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
@@ -37,21 +39,30 @@ class UploadController extends Controller
         $pathToFile = $request->pdfile->storeAs(config('folderId'), $filename, 'google');
 
 
-        
+        $client = new Google_Client(['verify' => false]);
+        $client->setClientId(config('clientId'));
+        $client->setClientSecret(config('clientSecret'));
+        $client->refreshToken(config('refreshToken'));
+
+        $service = new \Google_Service_Drive($client);
+    
         // This is uploading a file directly, with no metadata associated.
-        // $file = new Google_Service_Drive_DriveFile();
-        // $result = $service->files->create(
-        //     $file,
-        //     array(
-        //         'data' => file_get_contents(TESTFILE),
-        //         'mimeType' => 'application/octet-stream',
-        //         'uploadType' => 'media'
-        //     )
-        // );
+        $file = new Google_Service_Drive_DriveFile();
+        $result = $service->files->create(
+            $file,
+            array(
+                'data' => file_get_contents($request->file('pdfile')),
+                'mimeType' => 'application/octet-stream',
+                'uploadType' => 'media'
+            )
+        );
+
+        // $adapter = new GoogleDriveAdapter($service, config('folderId'));
+        
 
         // store localy
         // $pathToFile = $request->pdfile->storeAs('files', $filename, 'public');
 
-        return view('files.show')->with(['file'=>$pathToFile]);
+        return view('files.show')->with(['fileId'=>$result->id]);
     }
 }
